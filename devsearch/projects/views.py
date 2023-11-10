7f04c4868442e1ab4f1a3d7b9dd376999189c19b
FarishtaJayas <farishtajayas@gmail.com>
2023-11-11 02:04:31 +0600

    Added links using name on django template

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,17 +2,47 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'dev website',
+        'description': 'I am doing an awesome project',
+    },
+    {
+        'id': '2',
+        'title': 'mobile app development',
+        'description': 'Creating a cutting-edge mobile application.',
+    },
+    {
+        'id': '3',
+        'title': 'data science project',
+        'description': 'Analyzing large datasets to extract valuable insights.',
+    },
+    {
+        'id': '4',
+        'title': 'e-commerce platform',
+        'description': 'Building an online marketplace for various products.',
+    },
+    # Add more entries as needed
+]
+
 
 def projects(request):
-    number = 11
+    number = 8
     page = 'Page 1'
-    context = {'page': page, 'number': number}
+    context = {'page': page, 'number': number, 'projectList': projectsList}
     return render(request, 'projects/projects.html', {
         'context': context
     })
 
 
 def project(request, pk):
+    projectObj = None
+
+    for project in projectsList:
+        if (project['id'] == pk):
+            projectObj = project
     return render(request, 'projects/single-project.html', {
-        'code': pk
+        'code': pk,
+        'project': projectObj
     })
