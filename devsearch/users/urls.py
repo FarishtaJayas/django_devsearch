2d3be0c719b8c0f6ff35cec5006480fb7d538c61
FarishtaJayas <farishtajayas@gmail.com>
2023-11-21 01:38:27 +0600

    finished till adding adding skill and updating skill

--- devsearch/users/urls.py
+++ devsearch/users/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('register/', registerUser, name='register'),
     path('', profiles, name='profiles'),
     path('account/', userAccount, name='account'),
-    path('profile/<uuid:pk>/', userProfile, name='user-profile')
+    path('profile/<uuid:pk>/', userProfile, name='user-profile'),
+    path('edit-account/', editAccount, name='edit-account'),
+
+    path('create-skill', createSkill, name='create-skill'),
+    path('update-skill/<uuid:pk>/', updateSkill, name='update-skill'),
 ]
--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,10 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 # from django.contrib.auth.forms import UserCreationForm
 from .models import *
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -55,7 +56,7 @@ def registerUser(request):
             messages.success(request, "User account was created")
 
             login(request, user)
-            return redirect('profiles')
+            return redirect('edit-account')
         else:
             messages.error(
                 request, "An error has occurred during registration")
@@ -93,3 +94,53 @@ def userAccount(request):
         'projects': projects,
     }
     return render(request, 'users/account.html', context)
+
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
+
+
+@login_required(login_url='login')
+def createSkill(request):
+    profile = request.user.profile
+    form = SkillForm()
+
+    if request.method == 'POST':
+        form = SkillForm(request.POST)
+        if form.is_valid():
+            skill = form.save(commit=False)
+            skill.owner = profile
+            skill.save()
+            return redirect('account')
+
+    context = {'form': form}
+    return render(request, 'users/skill_form.html', context)
+
+
+@login_required(login_url='login')
+def updateSkill(request, pk):
+    profile = request.user.profile
+    skill = profile.skill_set.get(id=pk)
+    form = SkillForm(instance=skill)
+
+    if request.method == 'POST':
+        form = SkillForm(request.POST, instance=skill)
+        if form.is_valid():
+            form.save()
+            return redirect('account')
+    else:
+        context = {'form': form}
+        return render(request, 'users/skill_form.html', context)
+
+    return HttpResponse("Invalid request method")
